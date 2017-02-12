e82e0804fb9129022af0f1a3c85d75c03b3f5537
Alex-Joseph <alex.cheng@ryerson.ca>
2017-02-12 20:43:12 +0000

    exercise complete

--- https_example.js
+++ https_example.js
@@ -1,12 +1,17 @@
 var https = require('https');
 var options = {
-  host: 'www.youtube.com',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
 // called by https when the request is made.
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
